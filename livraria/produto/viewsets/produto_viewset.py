c9651cba11d95337f5db43fcf33da0fdab8ac9d7
Fabio Kauan <Fabiokauanfg@gmail.com>
2023-05-14 22:32:16 -0300

    tdd

--- livraria/produto/viewsets/produto_viewset.py
+++ livraria/produto/viewsets/produto_viewset.py
@@ -1,12 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 
 from produto.models import Produto
 from produto.serializers.produto_serializer import ProdutoSerializer
 
 class ProdutoViewSet(ModelViewSet):
-    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication]
 
     serializer_class = ProdutoSerializer
     queryset = Produto.objects.all()
